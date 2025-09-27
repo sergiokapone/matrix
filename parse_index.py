ce87179360d532ca2bbdcd414f6822d203213143
sergiokapone <sergiy.ponomarenko@gmail.com>
2025-09-27 17:06:15 +0300

    add scripts

---
+++ parse_index.py
@@ -0,0 +1,24 @@
+import re
+from pathlib import Path
+
+index_file = Path("disciplines/index.html")
+
+from wp_links import WP_LINKS
+
+
+html = index_file.read_text(encoding="utf-8")
+
+# regex шукає href на файли, що починаються з ЗО_ або ПО_
+pattern = re.compile(r'href="((ЗО|ПО)[ _]\d{2})\.html"')
+
+def replace_href(match):
+    code = match.group(1).replace('_', ' ').strip()
+    print(f"DEBUG: '{code}'")  # подивитися точний текст
+    wp_url = WP_LINKS.get(code, "#")
+    print(f"DEBUG wp_url: '{wp_url}'")
+    return f'href="{wp_url}"'
+
+html_new = pattern.sub(replace_href, html)
+
+index_file.write_text(html_new, encoding="utf-8")
+print("✅ href у index.html замінено на WP-посилання для ЗО_XX / ПО_XX")
---
+++ wp_links.py
@@ -0,0 +1,3 @@
+WP_LINKS = {
+    "ПО 01": "https://apd.ipt.kpi.ua/osvita/ok_e6-master-2025/po-01-kvantova-khimiia-i-kvantovo-mekhanichni-metodi-obchislennia/",
+}
